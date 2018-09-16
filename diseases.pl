adaba06a47df1f0321741b952a3dcc02a9e26607
Matthew King <32076099+mking047@users.noreply.github.com>
2018-09-16 10:52:08 -0400

    Add files via upload

---
+++ diseases.pl
@@ -0,0 +1,522 @@
+symptom(typhoid,myalgia).
+symptom(typhoid,bloating).
+symptom(typhoid,constipation).
+symptom(typhoid,nausea).
+symptom(typhoid,vomiting).
+symptom(typhoid,fatigue).
+symptom(typhoid,fever).
+symptom(typhoid,chills).
+symptom(typhoid,loss_of_appetite).
+symptom(typhoid,malaise).
+symptom(hepatitis_b,tired).
+symptom(hepatitis_b,fever).
+symptom(hepatitis_b,headache).
+symptom(hepatitis_b,loss_of_appetite).
+symptom(hepatitis_b,vomiting).
+symptom(hepatitis_b,stomach_pain).
+symptom(hepatitis_b,tan_stool).
+symptom(hepatitis_b,dark_urine).
+symptom(hepatitis_b,jaundice).
+symptom(hsv1,fever).
+symptom(hsv-1,swollen_lymph_nodes).
+symptom(hsv-1,tingling).
+symptom(hsv-1,sore_throat).
+symptom(hsv-1,dry_cough).
+symptom(hsv-1,rash).
+symptom(meningitis,headache).
+symptom(meningitis,fever).
+symptom(meningitis,stiff_neck).
+symptom(meningitis,loss_of_appetite).
+symptom(meningitis,sleepiness).
+symptom(meningitis,vomiting).
+symptom(rabies,loss_of_appetite).
+symptom(rabies,fever).
+symptom(rabies,headache).
+symptom(rabies,excesssalivation).
+symptom(rabies,musclespasms).
+symptom(rabies,paralysis).
+symptom(rabies,confusion).
+symptom(rabies,hyperactivity).
+symptom(rabies,hallucination).
+symptom(rabies,hydrophobia).
+symptom(diphtheria,sorethroat).
+symptom(diphtheria,fever).
+symptom(diphtheria,malaise).
+symptom(diphtheria,swollen_lymph_nodes).
+symptom(diphtheria,nasal_discharge).
+symptom(diphtheria,impaired_voice).
+symptom(diphtheria,rash).
+symptom(cellulitis,swollen_lymph_nodes).
+symptom(cellulitis,swelling_skin).
+symptom(cellulitis,redness_of_skin).
+symptom(cellulitis,fever).
+symptom(mononucleosis,swollen_lymph_nodes).
+symptom(mononucleosis,sore_throat).
+symptom(mononucleosis,headache).
+symptom(mononucleosis,abdominalpain).
+symptom(mononucleosis,fever).
+symptom(tularemia,swollen_lymph_nodes).
+symptom(tularemia,loss_of_appetite).
+symptom(tularemia,fever).
+symptom(tularemia,signs_of_sepsis).
+symptom(tularemia,ulcers).
+symptom(chickenpox,fever).
+symptom(chickenpox,tired).
+symptom(chickenpox,headache).
+symptom(chickenpox,pneumonia).
+symptom(chickenpox,inflammation_of_brain).
+symptom(chickenpox,bacterial_skin_infection).
+symptom(chickenpox,rash).
+symptom(measles,fever).
+symptom(measles,cough).
+symptom(measles,runny_nose).
+symptom(measles,inflammationofeyes).
+symptom(measles,rash).
+symptom(erythema_infectiosum,fever).
+symptom(erythema_infectiosum,headache).
+symptom(erythema_infectiosum,coryza).
+symptom(erythema_infectiosum,rash).
+symptom(erythema_infectiosum,abdominal_pain).
+symptom(erythema_infectiosum,pruritus).
+symptom(zika,fever).
+symptom(zika,rash).
+symptom(zika,joint_pain).
+symptom(zika,headache).
+symptom(zika,pink_eye).
+symptom(zika,myalgia).
+symptom(african_trypanosomiasis,fever).
+symptom(african_trypanosomiasis,headache).
+symptom(african_trypanosomiasis,joint_pain).
+symptom(african_trypanosomiasis,itching).
+symptom(african_trypanosomiasis,confusion).
+symptom(african_trypanosomiasis,tremors).
+symptom(african_trypanosomiasis,weakness).
+symptom(african_trypanosomiasis,paralysis).
+symptom(pneumonic_plague,fever).
+symptom(pneumonic_plague,weakness).
+symptom(pneumonic_plague,headach).
+symptom(pneumonic_plague,nausea).
+symptom(pneumonic_plague,shortness_of_breath).
+symptom(pneumonic_plague,chest_pain).
+symptom(pneumonic_plague,cough).
+symptom(lyme_disease,fever).
+symptom(lyme_disease,headache).
+symptom(lyme_disease,rash).
+symptom(lyme_disease,myalgia).
+symptom(lyme_disease,joint_pain).
+symptom(lyme_disease,heart_palpitation).
+symptom(lyme_disease,numbness).
+symptom(rocky_mountain_spotted_fever,fever).
+symptom(rocky_mountain_spotted_fever,headache).
+symptom(rocky_mountain_spotted_fever,rash).
+symptom(rocky_mountain_spotted_fever,nausea).
+symptom(rocky_mountain_spotted_fever,vomiting).
+symptom(rocky_mountain_spotted_fever,myalgia).
+symptom(rocky_mountain_spotted_fever,joint_pain).
+symptom(rocky_mountain_spotted_fever,abdominal_pain).
+symptom(rocky_mountain_spotted_fever,diarrhea).
+symptom(heartland_virus,fever).
+symptom(heartland_virus,headache).
+symptom(heartland_virus,fatigue).
+symptom(heartland_virus,nausea).
+symptom(heartland_virus,diarrhea).
+symptom(heartland_virus,myalgia).
+symptom(heartland_virus,joint_pain).
+symptom(heartland_virus,low_white_blood_cell_count).
+symptom(heartland_virus,low_platelet_count).
+symptom(severe_acute_respiratory_syndrome,fever).
+symptom(severe_acute_respiratory_syndrome,myalgia).
+symptom(severe_acute_respiratory_syndrome,tired).
+symptom(severe_acute_respiratory_syndrome,cough).
+symptom(severe_acute_respiratory_syndrome,sore_throat).
+symptom(cholera,diarrhea).
+symptom(cholera,vomiting).
+symptom(cholera,muscle_cramps).
+symptom(bhf,fever).
+symptom(bhf,malaise).
+symptom(bhf,headache).
+symptom(bhf,nose_bleeds).
+symptom(bhf,blood_spots).
+symptom(ahf,fever).
+symptom(ahf,malaise).
+symptom(ahf,headache).
+symptom(ahf,dizziness).
+symptom(ahf,blood_spots).
+symptom(ebola,fever).
+symptom(ebola,malaise).
+symptom(ebola,headache).
+symptom(ebola,chills).
+symptom(ebola,myalgia).
+symptom(mumps,fever).
+symptom(mumps,malaise).
+symptom(mumps,headache).
+symptom(mumps,parotid_gland_swelling).
+symptom(yellow_fever,fever).
+symptom(yellow_fever,headache).
+symptom(yellow_fever,chills).
+symptom(yellow_fever,back_pain).
+symptom(yellow_fever,fatigue).
+symptom(yellow_fever,loss_of_appetite).
+symptom(yellow_fever,myalgia).
+symptom(yellow_fever,nausea).
+symptom(yellow_fever,vomiting).
+symptom(west_nile_fever,fever).
+symptom(west_nile_fever,headache).
+symptom(west_nile_fever,chills).
+symptom(west_nile_fever,excessive_sweating).
+symptom(west_nile_fever,weakness).
+symptom(west_nile_fever,fatigue).
+symptom(west_nile_fever,swollen_lymph_nodes).
+symptom(west_nile_fever,drowsiness).
+symptom(west_nile_fever,joint_pain).
+symptom(west_nile_fever,nausea).
+symptom(west_nile_fever,vomiting).
+symptom(west_nile_fever,loss_of_appetite).
+symptom(west_nile_fever,diarrhea).
+symptom(kuru,ataxia).
+symptom(kuru,dysarthria).
+symptom(kuru,tremors).
+symptom(kuru,joint_pain).
+symptom(kuru,dementia).
+symptom(gerstmann-sträussler-scheinker_syndrome,ataxia).
+symptom(gerstmann-sträussler-scheinker_syndrome,dementia).
+symptom(gerstmann-sträussler-scheinker_syndrome,dysarthia).
+symptom(creutzfeldt-jakob_disease,dementia).
+symptom(creutzfeldt-jakob_disease,ataxia).
+symptom(creutzfeldt-jakob_disease,hallucinations).
+symptom(greyscale,psychosis).
+symptom(greyscale,hallucination).
+symptom(greyscale,mania).
+symptom(greyscale,confusion).
+symptom(greyscale,calciphylaxis).
+symptom(greyscale,skin_necrosis).
+symptom(neonatal_lupus,rash).
+symptom(neonatal_lupus,low_platelet_count).
+symptom(psoriasis,skin_rash).
+symptom(psoriasis,scaly_skin).
+symptom(lichen_planus,skin_rash).
+symptom(lichen_planus,ulcers).
+symptom(leprosy,lesion).
+symptom(leprosy,muscle_weakness).
+symptom(leprosy,enlarged_nerves).
+symptom(leprosy,dry_skin).
+symptom(filariasis,lymphedema).
+symptom(chikungunya,fever).
+symptom(chikungunya,joint_pain).
+symptom(chikungunya,headache).
+symptom(chikungunya,myalgia).
+symptom(chikungunya,rash).
+symptom(chikungunya,joint_swelling).
+symptom(hepatitis_c,fever).
+symptom(hepatitis_c,joint_pain).
+symptom(hepatitis_c,vomiting).
+symptom(hepatitis_c,nausea).
+symptom(hepatitis_a,fever).
+symptom(hepatitis_a,nausea).
+symptom(hepatitis_a,vomiting).
+symptom(hepatitis_a,diarrhea).
+symptom(gonorrhea,fever).
+symptom(gonorrhea,sore_throat).
+symptom(gonorrhea,vomiting).
+symptom(gonorrhea,vaginal_discharge).
+symptom(aquarium_granuloma,papules).
+symptom(aquarium_granuloma,vesicles).
+symptom(mycobacterium_shottsii,nodules).
+symptom(buruli_ulcer,papules).
+symptom(buruli_ulcer,ulcers).
+symptom(apha_gal,mammallian_meat_allergy).
+symptom(apha_gal,rash).
+symptom(alpha_gal,hives).
+symptom(apha_gal,anaphylaxis).
+
+
+
+
+
+locale(typhoid,central_asia).
+locale(typhoid,east_asia).
+locale(typhoid,north_america).
+locale(hepatitis_b,africa).
+locale(hepatitis_b,east_asia).
+locale(hsv-1,_).
+locale(rocky_mountain_spotted_fever,north_america).
+locale(rocky_mountain_spotted_fever,central_america).
+locale(rocky_mountain_spotted_fever,south_america).
+locale(heartland_virus,north_america).
+locale(chickenpox,everywhere).
+locale(measles,europe).
+locale(measles,central_asia).
+locale(measles,east_asia).
+locale(measles,north_america).
+locale(measles,africa).
+locale(erythema_infectiosum,north_america).
+locale(erythema_infectiosum,europe).
+locale(bhf,south_america).
+locale(ahf,south_america).
+locale(ebola,africa).
+locale(greyscale,africa).
+locale(zika,africa).
+locale(zika,south_america).
+locale(african_trypanosomiasis,africa).
+locale(pneumonic_plague,_).
+locale(aquarium_granuloma,_).
+locale(mycobacterium_shottsii,north_america).
+locale(buruli_ulcer,africa).
+locale(alpha_gal,north_america).
+
+treatment(hepatitis_c,anitiviral_drug).
+treatment(hepatitis_c,liver_transplant).
+treatment(hepatitis_a,rest).
+treatment(hepatitis_a,rehydration).
+treatment(gonorrhea,antibiotics).
+treatment(hepatitis_b,none).
+treatment(typhoid,antibiotics).
+treatment(hsv-1,anti-viral).
+treatment(meningitis,antibiotics).
+treatment(meningitis,steroid).
+treatment(meningitis,penicillin).
+treatment(meningitis,hospitalization).
+treatment(meningitis,oxygen_therapy).
+treatment(rabies,blood_transfusion).
+treatment(rabies,antiviral_drug).
+treatment(diphtheria,antibiotics).
+treatment(diphtheria,penicillin).
+treatment(diphtheria,antitoxin).
+treatment(cellulitis,antibiotics).
+treatment(cellulitis,surgical_drainage_of_pus).
+treatment(mononucleosis,paracetamol).
+treatment(mononucleosis,nsaids).
+treatment(tularemia,doxycycline).
+treatment(tularemia,gentamicin).
+treatment(tularemia,streptomycin).
+treatment(tularemia,ciprofloxacin).
+treatment(chickenpox,antihistamines).
+treatment(chickenpox,topical_ointments).
+treatment(measles,drinking_fluids).
+treatment(measles,rest).
+treatment(erythema_infectiosum,acetaminophen).
+treatment(erythema_infectiosum,rest).
+treatment(erythema_infectiosum,drinking_fluids).
+treatment(zika,rest).
+treatment(african_trypanosomiasis,intravenous_pentamidine).
+treatment(african_trypanosomiasis,intramuscular_pentamidine).
+treatment(african_trypanosomiasis,nifurtimox).
+treatment(african_trypanosomiasis,eflornithine).
+treatment(pneumonic_plague,streptomycin).
+treatment(pneumonic_plague,gentamicin).
+treatment(pneumonic_plague,tetracyclines).
+treatment(pneumonic_plague,chloramphenicol).
+treatment(lyme_disease,amoxicillin).
+treatment(lyme_disease,tetracycline).
+treatment(rocky_mountain_spotted_fever,tetracycline).
+treatment(rocky_mountain_spotted_fever,chloramphenicol).
+treatment(heartland_virus,none).
+treatment(severe_acute_respiratory_syndrome,antipyretics).
+treatment(severe_acute_respiratory_syndrome,mechanical_ventilation).
+treatment(cholera,oral_rehydration_therapy).
+treatment(cholera,zinc_supplementation).
+treatment(cholera,antibiotics).
+treatment(cholera,intravenous_fluids).
+treatment(bhf,vesper_mice_reduced_contact).
+treatment(bhf,junin_vaccination).
+treatment(ahf,junin_vaccination).
+treatment(ebola,blood_transfusion).
+treatment(ebola,iv_fluids).
+treatment(ebola,oxygen_therapy).
+treatment(mumps,icing_neck).
+treatment(mumps,heating_neck).
+treatment(mumps,acetaminophin).
+treatment(yellow_fever,hospitalization).
+treatment(west_nile_fever,hospitalization).
+treatment(west_nile_fever,intravenous_fluids).
+treatment(west_nile_fever,respiratory_support).
+treatment(kuru,none).
+treatment(kuru,symptom_management).
+treatment(gerstmann-sträussler-scheinker_syndrome,none).
+treatment(gerstmann-sträussler-scheinker_syndrome,symptom_management).
+treatment(creutzfeldt-jakob_disease,none).
+treatment(creutzfeldt-jakob_disease,symptom_management).
+treatment(greyscale,prayer).
+treatment(greyscale,sacrifice).
+treatment(greyscale,fasting).
+treatment(neonatal_lupus,topical_steroids).
+treatment(psoriasis,corticosteroids).
+treatment(lichen_planus,corticosteroids).
+treatment(chikungunya,none).
+treatment(filariasis,antibiotics).
+treatment(filariasis,ivermectin).
+treatment(filariasis,albendazole).
+treatment(leprosy,antibiotics).
+treatment(aquarium_granuloma,antimycobacterials).
+treatment(aquarium_granuloma,hot_compresses).
+treatment(aquarium_granuloma,surgical_removal).
+treatment(mycobacterium_shottsi,antimycobacterials).
+treatment(mycobacterium_shottsi,hot_compresses).
+treatment(buruli_ulcer,antimycobacterials).
+treatment(buruli_ulcer,hot_compresses).
+treatment(buruli_ulcer,surgical_removal).
+treatment(alpha_gal,become_vegetarian).
+
+
+
+test(hepatitis_b,blood).
+test(hsv-1,blood).
+test(hsv-1,physical_examination).
+test(meningitis,bloodcultures).
+test(meningitis,computerizedtomography).
+test(meningitis,lumbarpuncture).
+test(rabies,siliva).
+test(rabies,lumbar_puncture).
+test(rabies,skin_biopsy).
+test(diphtheria,toxigenicity).
+test(diphtheria,polymerase_chain_reaction(pcr)).
+test(diphtheria,serologic_testing).
+test(cellulitis,complete_blood_count(cbc)).
+test(cellulitis,blood_culture).
+test(mononucleosis,mononuclear_spot_test).
+test(mononucleosis,ebv_antibody_test).
+test(tularemia,blood_test_for_f_tularensis).
+test(tularemia,sputum_culture).
+test(chickenpox,varicella_test).
+test(chickenpox,elisa_test).
+test(chickenpox,physical_examination).
+test(measles,rubeola_antibody_blood_test).
+test(measles,measles_immunity_test).
+test(measles,physical_examination).
+test(erythema_infectiosum,parvovirus_b19_blood_test).
+test(erythema_infectiosum,physical_examination).
+test(african_trypanosomiasis,trypanosomes_in_blood).
+test(zika,trioplex_rrt-pcr).
+test(zika,zika_mac-elisa).
+test(pneumonic_plague,blood).
+test(pneumonic_plague,lymph_nodes).
+test(pneumonic_plague,lungs).
+test(lyme_disease,elisa).
+test(lyme_disease,western_blot).
+test(rocky_mountain_spotted_fever,ifa).
+test(rocky_mountain_spotted_fever,serology).
+test(rocky_mountain_spotted_fever,skin_biopsy).
+test(heartland_virus,none).
+test(severe_acute_respiratory_syndrome,elisa).
+test(severe_acute_respiratory_syndrome,immunofluorescence).
+test(severe_acute_respiratory_syndrome,pcr).
+test(severe_acute_respiratory_syndrome,chest_x-ray).
+test(cholera,stool_test).
+test(bhf,elisa).
+test(bhf,igm).
+test(bhf,igg).
+test(ahf,rtpcr).
+test(ahf,elisa).
+test(ebola,blood_test).
+test(ebola,rptcr).
+test(ebola,elisa).
+test(mumps,elisa).
+test(mumps,ifa).
+test(mumps,igm).
+test(yellow_fever,elisa).
+test(yellow_fever,igm).
+test(yellow_fever,igg).
+test(yellow_fever,mia).
+test(west_nile_fever,physical_examination).
+test(west_nile_fever,spinal_tap).
+test(west_nile_fever,eeg).
+test(west_nile_fever,mri).
+test(west_nile_fever,csf).
+test(west_nile_fever,igm).
+test(west_nile_fever,igg).
+test(kuru,neurological_exam).
+test(kuru,bloodtest).
+test(gerstmann-sträussler-scheinker_syndrome,genetic_testing).
+test(gerstmann-sträussler-scheinker_syndrome,nervous_system_test).
+test(creutzfeldt-jakob_disease,electroencephalogram).
+test(creutzfeldt-jakob_disease,spinal_fluid_test).
+test(greyscale,skin_biopsy).
+test(typhoid,blood).
+test(chikungunya,elisa).
+test(filariasis,serology).
+test(filariasis,microscopic_testing).
+test(leprosy,skin_biopsy).
+test(leprosy,nerve_biopsy).
+test(hepatitis_c,hcv_rna_tests).
+test(hepatitis_a,hav_igm_antibody_test).
+test(gonorrhea,nucleic_acid_amplification).
+test(gonorrhea,urine_sample).
+test(buruli_ulcer,gram_stain).
+test(buruli_ulcer,photochromogenicity).
+test(mycobacterium_shottsi,gram_stain).
+test(mycobacterium_shottsi,gene_sequence).
+test(mycobacterium_shottsi,photochromogenicity).
+test(aquarium_granuloma,gram_stain).
+test(aquarium_granuloma,photochromogenicity).
+test(alpha_gal,blood).
+
+
+
+
+print( [ ] ).
+
+print( [ X | Y ] ):- nl, write(X), print( Y ).
+
+inter([], _, []).
+
+inter([H1|T1], L2, [H1|Res]) :-
+	member(H1, L2),
+	inter(T1, L2, Res).
+
+inter([_|T1], L2, Res) :-
+	inter(T1, L2, Res).
+
+
+start() :-
+	nl,
+	write('Enter a symptom \n'),
+	read(Y),
+	findall(X, symptom(X, Y) , Z),
+	write('list of possibilities: \n'),
+	print(Z),
+	nl,
+	searchAgain(Z).
+
+searchAgain(Z) :-
+	(length(Z, 0) -> exit();
+	nl,
+	write('Enter another symptom or type "1" to list treatments/tests for given possibilities  \n'),
+	read(J),
+	(J==1 -> printTreatment(Z), printTest(Z);
+	findall(X, symptom(X, J) , Q),
+	inter(Z, Q, M),
+	write('list of possibilities: \n'),
+	print(M),
+	nl,
+	searchAgain(M))).
+
+printTreatment([]) :-
+	nl.
+printTreatment([X|Y]) :-
+	findall(R, treatment(X, R) , Q),
+	nl,
+	write('Treatments for '),
+	write(X),
+	write(':'),
+	print(Q),
+	nl,
+	printTreatment(Y).
+
+printTest([]) :-
+	nl,
+	write('Thanks for using the medical diagnosis application \n').
+printTest([X|Y]) :-
+	findall(V, test(X, V), U),
+	nl,
+	write('Tests for '),
+	write(X),
+	write(':'),
+	print(U),
+	nl,
+	printTest(Y).
+
+exit() :-
+	write('Could not diagnose the disease, sorry :( \n').
+
+
+	
